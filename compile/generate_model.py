4ae5b85f8079b27aae0a64be7d9cd11ae9ae8001
George Hotz <george@comma.ai>
2021-07-04 10:11:30 -0700

    compile edgetpu model

---
+++ compile/generate_model.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import numpy as np
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+import tensorflow as tf
+# example from https://www.tensorflow.org/lite/convert
+
+# Create a model using high-level tf.keras.* APIs
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(units=1, input_shape=[1]),
+    tf.keras.layers.Dense(units=16, activation='relu'),
+    tf.keras.layers.Dense(units=1)
+])
+model.compile(optimizer='sgd', loss='mean_squared_error') # compile the model
+model.fit(x=[-1, 0, 1], y=[-3, -1, 1], epochs=5) # train the model
+# (to generate a SavedModel) tf.saved_model.save(model, "saved_model_keras_dir")
+
+def representative_dataset():
+  for _ in range(100):
+    data = np.random.rand(1, 1)
+    yield [data.astype(np.float32)]
+
+# Convert the model.
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_dataset
+
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.int8  # or tf.uint8
+converter.inference_output_type = tf.int8  # or tf.uint8
+tflite_model = converter.convert()
+
+# Save the model.
+with open('model.tflite', 'wb') as f:
+  f.write(tflite_model)
+
