10d1512f0ba65acfe6705712e24a6b672ed7dd05
George Hotz <geohot@gmail.com>
2022-10-31 23:07:05 -0700

    programs

--- compile/generate_model.py
+++ compile/generate_model.py
@@ -27,8 +27,8 @@ def representative_dataset():
 # Convert the model.
 root = tf.train.Checkpoint()
 #root.f = tf.function(lambda x: tf.nn.relu(x))
-root.f = tf.function(lambda x: 2*x)
-input_data = tf.constant(1., shape=[1, 1])
+root.f = tf.function(lambda x: tf.nn.relu(x))
+input_data = tf.constant(1., shape=[4])
 to_save = root.f.get_concrete_function(input_data)
 
 converter = tf.lite.TFLiteConverter.from_concrete_functions([to_save])
---
+++ programs/compare.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+mul2 = open("relu_2.coral", "rb").read()
+relu = open("relu_4.coral", "rb").read()
+from termcolor import colored
+
+assert len(mul2) == len(relu)
+
+app = []
+for i in range(len(mul2)):
+  if i%0x10 == 0 and i != 0:
+    app.append("\n")
+  dat = "%02X" % mul2[i]
+  dat2 = "%02X" % relu[i]
+  if mul2[i] != relu[i]:
+    app.append(' '+colored(dat, 'red')+''+colored(dat2, 'green')+' ')
+  else:
+    app.append('  '+dat+'  ')
+
+print(''.join(app))
