51eb2ceda175bbaec01f34a755ce8bfc6d052901
George Hotz <geohot@gmail.com>
2022-11-01 20:24:57 -0700

    progress

--- decompiler.py
+++ decompiler.py
@@ -7,46 +7,15 @@ from collections import defaultdict
 from colored import stylize, fg
 
 dat = open("programs/div2.coral", "rb").read()
+#altdat = []
+#dat = open("programs/inception_0.coral", "rb").read()
 altdat = open("programs/relu.coral", "rb").read()
 #dat = open("programs/weight_copy_in_0x80.coral", "rb").read()
 #dat = open("programs/dense_1_8_mul.coral", "rb").read()
 
-# div2_20 vs div2_10
-# 150                                      prefix:     680,imm_size:      80,    v_op:       e,v_offset:     5ff,  vs_reg:       2,    s_op:       0,     s_x:       0,     s_y:       0,imm_scalar:   20002,   unk_3:       0                                  
-# 150                                      prefix:     280,imm_size:      80,    v_op:       f,v_offset:     5ff,  vs_reg:       2,    s_op:       0,     s_x:       0,     s_y:       0,imm_scalar:   20002,   unk_3:       0                                                                                           
-
-
-
-# div2 vs relu vs add1
-# 6B0       prefix:   18600,imm_size:       0,    v_op:       0,v_offset:    1dfe,  vs_reg:       f,    s_op:      1f,     s_x:       0,     s_y:       0,imm_scalar:  21bf80,   unk_3:       0
-# 6B0       prefix:   18600,imm_size:       0,    v_op:       0,v_offset:       0,  vs_reg:      18,    s_op:      1f,     s_x:       0,     s_y:       0,imm_scalar:  21bf80,   unk_3:       0
-# 6B0       prefix:   18600,imm_size:     380,    v_op:       f,v_offset:    1dff,  vs_reg:      17,    s_op:      1f,     s_x:      1e,     s_y:      1f,imm_scalar:  21bf7f,   unk_3:       0                                        
-
-# add1 vs sub1
-# 3C0       prefix:   1ffb4,imm_size:     1ff,    v_op:       0,v_offset:    1f00,  vs_reg:      1f,    s_op:      3f,     s_x:       1,     s_y:       0,imm_scalar:   7fe00,   unk_3:       0
-# 3C0       prefix:   1ffb4,imm_size:     1ff,    v_op:       0,v_offset:       0,  vs_reg:       0,    s_op:       0,     s_x:       0,     s_y:       0,imm_scalar:   7fe00,   unk_3:      20
-
-# 0x10 send
-# 2D0       prefix:    3000,imm_size:      20,    v_op:       0,v_offset:     300,  vs_reg:       0,    s_op:       0,     s_x:      1d,     s_y:      1f,imm_scalar:fe80007f,   unk_3: 10000ff
-# 2E0 7     prefix:     3ff,imm_size:     200,    v_op:       e,v_offset:     fff,  vs_reg:       0,    s_op:       0,     s_x:      10,     s_y:      1e,imm_scalar: 80007ff,   unk_3:     402
-# 7B0       prefix:      a6,imm_size:       0,    v_op:       0,v_offset:       0,  vs_reg:       2,    s_op:       0,     s_x:       0,     s_y:      18,imm_scalar: 6000100,   unk_3: 3fffa00
-# 7C0       prefix:   1e800,imm_size:     3ff,    v_op:       1,v_offset:       0,  vs_reg:      1d,    s_op:      3f,     s_x:      1f,     s_y:       1,imm_scalar:1fffd000,   unk_3: 3ffa000
-
-# 0x20 send
-# 2D0       prefix:    3000,imm_size:      20,    v_op:       0,v_offset:     700,  vs_reg:       0,    s_op:       0,     s_x:      19,     s_y:      1f,imm_scalar:fc80007f,   unk_3: 10000ff                                        
-# 2E0       prefix:     3ff,imm_size:     200,    v_op:       c,v_offset:     fff,  vs_reg:       0,    s_op:       0,     s_x:      10,     s_y:      1c,imm_scalar: 80007ff,   unk_3:     402
-# 7B0       prefix:      a6,imm_size:       0,    v_op:       0,v_offset:       0,  vs_reg:       2,    s_op:       0,     s_x:       0,     s_y:      18,imm_scalar: e000100,   unk_3: 3fff200
-# 7C0       prefix:   1c800,imm_size:     3ff,    v_op:       1,v_offset:       0,  vs_reg:      19,    s_op:      3f,     s_x:      1f,     s_y:       1,imm_scalar:1fff9000,   unk_3: 3ff2000
-
-
 # CORAL NOTES
 # 32 scalar registers, 32-bits?
 # 8 predicate registers
-
-#def fix(y):
-#  return bytes([int("0x"+x, 16) for x in y.split()])
-#op = fix("00 08 00 00 00 00 C0 1B  00 AA AA AA AA 00 00 00")
-#print(op)
 
 ins = BitStruct(
   "unk_3" / BitsInteger(18),
@@ -61,10 +30,11 @@ ins = BitStruct(
 
   # unknown?
   "vs_reg" / BitsInteger(5),  # confirmed as reg
-  "v_offset" / BitsInteger(13),
+  "v_cmd" / BitsInteger(5),   # could be reg?
+  "v_offset" / BitsInteger(8),
   # v_op and imm_size are fused with prefix = 0x7c
-  "v_op" / BitsInteger(4),
-  "imm_size" / BitsInteger(13),
+  "v_op" / BitsInteger(5),
+  "imm_size" / BitsInteger(12),
   "vs_reg_v1" / BitsInteger(5),  # confirmed as reg
 
   # both of these seem gated on the preds
@@ -121,7 +91,7 @@ def dec(my_dat, off=-1, cmp=None):
     if dd['v_op'] == 6:
       more = "USB ACT"
     elif dd['enable_scalar'] and dd['branch'] == 0:
-      imm = f"0x{dd['imm_scalar']:X}" if dd['s_op'] & 0x20 else f"s{dd['imm_scalar']}"
+      imm = f"0x{dd['imm_scalar']:X}" if dd['s_op'] & 0x20 else f"s{dd['imm_scalar']&0x1F}"
       more = f"{'C' if dd['s_op']&0xF in [9,10,11,12,13,14] else 's'}{dd['s_x']} <- s{dd['s_y']} {ops[dd['s_op']&0xF]} {imm}"
     print("%-30s" % more, f','.join(prt))
 
@@ -199,10 +169,14 @@ prog = []
 #prog += mins(enable_scalar=0x1, s_op=MOVI, s_x=0xb, imm_scalar=0xabab, vs_reg=0, v_op_2=7, vs_reg_w=5)
 prog += mins(enable_scalar=0x1, s_op=MOVI, s_x=0, imm_scalar=0xabab)
 
-prog += mins(enable_scalar=1, enable_vector=1,
-  vs_reg_v1=0x12, imm_size=0xff, v_op=1, v_offset=1, vs_reg=1,
-  s_op=ADDI, s_x=4, s_y=1, imm_scalar=0xeeff,
-  unk_3=0x3ffff)
+prog += mins(enable_scalar=0x1, s_op=MOVI, s_x=0, imm_scalar=0xabab)
+
+#for i in range(8): prog += mins(v_op_2=i, vs_reg_w=0x10+i, imm_scalar=0x20000000, unk_3=0x20002)
+
+#prog += mins(enable_scalar=1, enable_vector=1,
+#  vs_reg_v1=0x12, imm_size=0xff, v_op=1, v_offset=1, vs_reg=1,
+#  s_op=ADDI, s_x=4, s_y=1, imm_scalar=0xeeff,
+#  unk_3=0x3ffff)
 #prog += mins(enable_vector=1, vs_reg_v1=0x13, unk_3=0x3ffff)
 #prog += mins(enable_vector=1, vs_reg_v1=0x14)
 #prog += mins(enable_scalar=0, vs_reg_v1=0x13, enable_vector=2, s_op=MOVI, s_x=0xb, imm_scalar=0xabab, vs_reg=0, v_op_2=0, vs_reg_w=0)
@@ -257,8 +231,8 @@ prog += mins(prefix=0x40, gate=1, pred_reg=7, yes_pred=0, s_op=MOVI, s_x=0x17, i
 #prog += mins(prefix=0x10, vs_reg=8, v_op=5, imm_offset=2)
 
 prog += mins(enable_scalar=0x1, s_op=MOVI, s_x=0x1b, imm_scalar=4)  # this one has a small range
-prog += mins(enable_scalar=0x1, vs_reg=0x1b, v_op=5, v_offset=3)
-prog += mins(enable_scalar=0x1, v_op=6)
+prog += mins(enable_scalar=0x1, vs_reg=0x1b, v_op=0xa, v_offset=3)
+prog += mins(enable_scalar=0x1, v_op=0xc)
 
 # send "output tensor" (EP 1)
 """
